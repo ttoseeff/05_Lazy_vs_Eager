0780c1a719c52242f2740b624f8e649fd223c3b2
Toseef Ahmad <Toseef.Ahmad@cireson.com>
2023-06-10 00:20:24 +0500

    Add project files.

---
+++ 05_Lazy_vs_Eager/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _04_Thread_Safety_Singleton;
+
+Console.WriteLine("Sigleton Program");
+Parallel.Invoke(new Action(() => PrintStudentDetails()), new Action(() => PrintEmployeeDetails()));
+
+static void PrintStudentDetails()
+{
+    Singleton FromStudent = Singleton.Instance;
+    FromStudent.PrintDetails("From Student");
+}
+
+static void PrintEmployeeDetails()
+{
+    Singleton FromEmployee = Singleton.Instance;
+    FromEmployee.PrintDetails("From Employee");
+}
+
+/*
+ * why we should use sealed keyword in the singleton class
+ * 
+ *Singleton.SingletonDerived singletonDerived = new Singleton.SingletonDerived();
+ *singletonDerived.PrintDetails("Derived Class");
+ *
+ */
---
+++ 05_Lazy_vs_Eager/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Thread_Safety_Singleton
+{
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+        private static readonly Lazy<Singleton> instance = new Lazy<Singleton>(() => new Singleton());
+        public static Singleton Instance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter Value: " + counter.ToString());
+        }
+
+        public void PrintDetails(string details)
+        {
+            Console.WriteLine(details);
+        }
+
+        /*
+         * why we should use sealed keyword in the singleton class
+         * 
+         *       public class SingletonDerived : Singleton
+         *       {
+         *       }
+         */
+    }
+}
